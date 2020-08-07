d7543301a1d38b748a9dcc7070f3e2ffc5349bc9
rizalphlv <Rizalphlv12@gmail.com>
2020-08-07 22:56:34 +0700

    add logout and post

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::namespace('Auth')->group(function(){
     Route::post('register','RegisterController'); 
     Route::post('login','LoginController'); 
+    Route::post('logout','LogoutController'); 
 });
 
+Route::namespace('Article')->middleware('auth:api')->group(function(){
+    Route::post('create-new-article','ArticleController@store');
+});
 Route::get('user','UserController');
